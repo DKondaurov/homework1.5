f5941094865e96683c96ea8af38403b543a4ce6e
homework <9874@bk.ru>
2021-09-13 14:48:42 +0300

    Homework 1.5 is  done

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -1,7 +1,6 @@
 package pro.sky.java.course1;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class Main {
 
@@ -13,13 +12,9 @@ public class Main {
         }
         return arr;
     }
+
     public static void main(String[] args) {
-        // Задание 1
-        task1();
-        // Задание 2
-        task2();
-        // Задание 3
-        task3();
+        taskFromTheFirstToTheThird();
         // Задание 4
         task4();
         // Задание 5
@@ -32,11 +27,21 @@ public class Main {
         task8();
         // Задание 9
         task9();
+    }
 
+    private static void task9() {
+        int[] arrayToSearch = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        for (int i = 0; i < arrayToSearch.length; i++) {
+            for (int i1 = i; i1 < arrayToSearch.length; i1++) {
+                if (arrayToSearch[i] + arrayToSearch[i1] == -2) {
+                    System.out.println("Сумма чилсел " + arrayToSearch[i] + " и " + arrayToSearch[i1] + " равно -2");
+                }
+            }
+        }
     }
 
     private static void task8() {
-        int [] arrayToSearch = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        int[] arrayToSearch = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
         boolean a = true;
         for (int i = 0; i < arrayToSearch.length && a; i++) {
             for (int i1 = i; i1 < arrayToSearch.length; i1++) {
@@ -48,37 +53,23 @@ public class Main {
         }
     }
 
-    private static void task9() {
-        int [] arrayToSearch = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
-        for (int i = 0; i < arrayToSearch.length; i++) {
-            for (int i1 = i; i1 < arrayToSearch.length; i1++) {
-                if (arrayToSearch[i] + arrayToSearch[i1] == -2) {
-                    System.out.println("Сумма чилсел " + arrayToSearch[i] + " и " + arrayToSearch[i1] + " равно -2");
-                }
-            }
-        }
-    }
-
     private static void task7() {
-        int [] array = {5, 8, 2, 3, 9,};
-        int length = array.length;
+        int[] array = {5, 8, 2, 3, 9};
         System.out.println(Arrays.toString(array));
-        System.out.print("[");
-        for (int i = 1; i < array.length; i++) {
-            System.out.print(array[length-1] + ", ");
-            length--;
+        for (int start = 0, finish = array.length - 1; start < finish; start++, finish--) {
+            int variable = array[start];
+            array[start] = array[finish];
+            array[finish] = variable;
         }
-        System.out.print(array[length-1] + "]");
-        System.out.println();
+        System.out.println(Arrays.toString(array));
     }
 
     private static void task6() {
         int [] array = {5, 4, 3, 2, 1};
-        int length = array.length;
-        int[] newArray = new int[length];
+        int length = array.length - 1;
+        int[] newArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            newArray[i] = array[length - 1];
-            length--;
+            newArray[i] = array[length - i];
         }
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(newArray));
@@ -86,11 +77,10 @@ public class Main {
 
     private static void task5() {
         int[][] matrix = new int[3][3];
-        int arrayLength = matrix.length - 1;
+        int arrayLong = matrix.length - 1;
         for (int i = 0; i < matrix.length; i++) {
             matrix[i][i] = 1;
-            matrix[i][arrayLength] = 1;
-            arrayLength--;
+            matrix[i][arrayLong - i] = 1;
         }
         for (int[] row : matrix) {
             for (int column : row) {
@@ -101,47 +91,38 @@ public class Main {
     }
 
     private static void task4() {
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        int arrayLength = reverseFullName.length;
-        for (int i = 0; i < reverseFullName.length; i++) {
-            System.out.print(reverseFullName[arrayLength-1]);
-            arrayLength--;
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length; i > 0; i--) {
+            System.out.print(reverseFullName[i - 1]);
         }
         System.out.println();
     }
 
-    private static void task3() {
-        float averageExpenditure = 0f;
-        int i = 0;
-        for (; i < generateRandomArray().length; i++) {
-            averageExpenditure += generateRandomArray()[i];
+    private static void taskFromTheFirstToTheThird() {
+        int[] arr = generateRandomArray();
+        // Задание 1
+        int amountPayments = 0;
+        for (int i = 0; i < arr.length; i++) {
+            amountPayments += arr[i];
         }
-        averageExpenditure /= i;
-        System.out.println("Средняя сумма трат за месяц составила " + averageExpenditure + " рублей.");
-    }
-
-    private static void task2() {
+        System.out.println(amountPayments);
+        // Задание 2
         int maximumPayout = 0;
         int minimumPayout = 200_000;
-        for (int i = 0; i < generateRandomArray().length; i++) {
-            if (minimumPayout > generateRandomArray()[i]) {
-                minimumPayout = generateRandomArray()[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (minimumPayout > arr[i]) {
+                minimumPayout = arr[i];
             }
-            for (int i1 = 0; i1 < generateRandomArray().length; i1++) {
-                if (maximumPayout < generateRandomArray()[i]) {
-                    maximumPayout = generateRandomArray()[i];
-                }
+            if (maximumPayout < arr[i]) {
+                maximumPayout = arr[i];
             }
         }
         System.out.println("Минимальная сумма трат за день составила " + minimumPayout + " рублей. Максимальная сумма" +
                 " трат за день составила " + maximumPayout + " рублей.");
-    }
-
-    private static void task1() {
-        int amountPayments = 0;
-        for (int i = 0; i < generateRandomArray().length; i++) {
-            amountPayments = generateRandomArray()[i] + amountPayments;
-        }
-        System.out.println(amountPayments);
+        /* Задание 3
+        Если в первом задании я считаю общую сумму, то можно использовать значение из первого задания и разделить длину.*/
+        float averageExpenditure = 0f;
+        averageExpenditure = amountPayments / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + averageExpenditure + " рублей.");
     }
 }
